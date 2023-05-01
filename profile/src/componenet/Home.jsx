45033b08b0eafa62aecd2980abf545f3940e21e9
Ajay A Adsule <103304466+AjayAdsule@users.noreply.github.com>
2023-05-02 03:25:05 +0530

    small change

--- profile/src/componenet/Home.jsx
+++ profile/src/componenet/Home.jsx
@@ -14,7 +14,9 @@ const Home = () => {
             I'm Full Stack web devloper The crossover between design and programming has always been of interest to me, I've been lucky enough to work alongside some talented teams on a number of high profile websites. I have a wide range of skills that include back-end development using open source technologies (NodeJs), design (working closely with designers), front-end development (React, Angular9, ReactJs, HTML5, CSS3, Javascript, Responsive, UX), database(MongoDB), CI/CD(Docker, kubernetes)
             </p>
           </div>
+         
         </div>
+        <h2 className='mx-[80px] my-[10px]'>Techonlogy I know</h2>
       </header>
       <Tech/>
     </React.Fragment>
